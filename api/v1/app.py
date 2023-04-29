57b3281850fa91c49f980830cc4e022bc4d32401
EBUKA EMMANUEL EJIE <tripleeoliver@gmail.com>
2023-04-29 11:06:20 -0700

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from models import storage
 from api.v1.views import app_views
 from os import environ
-from flask import Flask
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,6 +13,17 @@ app.register_blueprint(app_views)
 def close_db(error):
     """ Close Storage """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
