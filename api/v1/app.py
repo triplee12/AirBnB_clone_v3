ae2d6fba92365c182e2c7b98ad6fb09113e1f98d
EBUKA EMMANUEL EJIE <tripleeoliver@gmail.com>
2023-04-29 20:57:17 -0700

    Fix code

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,10 @@
 from models import storage
 from api.v1.views import app_views
 from os import environ
-from flask import Flask, jsonify, make_response, render_template
+from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
